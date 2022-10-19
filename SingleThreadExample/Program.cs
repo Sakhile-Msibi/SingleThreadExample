4a666822ceb7b65e9ee0e7cb0e1c56cba3142dbc
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-10-19 15:44:29 +0200

    Add project files.

---
+++ SingleThreadExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleThreadExample
+{
+    class Program
+    {
+        public void metho1()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Method1 is : {0}", i);
+
+                if (i == 5)
+                    Thread.Sleep(6000);
+            }
+        }
+
+        public void metho2()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Method2 is : {0}", i);
+            }
+        }
+    }
+
+    class DriverClass
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Program Geek = new Program();
+
+
+            Thread thr1 = new Thread(Geek.metho1);
+            Thread thr2 = new Thread(Geek.metho2);
+            thr1.Start();
+            thr2.Start();
+            //Geek.metho1();
+            //Geek.metho2();
+        }
+    }
+}
